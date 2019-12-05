1ab961ea10f5aae04a7d1ed615aef609e9ddb45d
WindZQ <1049498972@qq.com>
2019-12-05 21:07:07 +0800

    Chapter 2 Part Problem Solution

---
+++ Chapter_2_Variables_And_Basic_Types/2.34.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(void)
+{
+    int i = 0, &r = i;
+    auto a = r;
+
+    const int ci = i, &cr = ci;
+    auto b = ci;
+    auto c = cr;
+    auto d = &i;
+    auto e = &ci;
+
+    const auto f = ci;
+    auto &g = ci;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
+    std::cout << f << std::endl;
+    std::cout << g << std::endl;
+    std::cout << "-----------------" << std::endl;
+
+    a = 42, b = 42, c = 42;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
+    std::cout << f << std::endl;
+    std::cout << g << std::endl;
+    return 0;
+}
