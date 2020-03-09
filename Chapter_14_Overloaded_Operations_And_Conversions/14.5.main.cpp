404e4b964dda2305a922fb4047197de1015071df
WindZQ <1049498972@qq.com>
2020-03-09 20:01:54 +0800

    Chapter 14 Part Problem Solution

---
+++ Chapter_14_Overloaded_Operations_And_Conversions/14.5.main.cpp
@@ -0,0 +1,14 @@
+#include "14.5.h"
+
+int main(void)
+{
+	Book book1(123, "CP5", "Lippman", "2012");
+	Book book2(123, "CP5", "Lippman", "2012");
+
+	if (book1 == book2)
+	{
+		std::cout << book1 << std::endl;
+	}
+	
+	return 0;
+}
